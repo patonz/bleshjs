662ddbd865da25b690e278ea4f9bf720a2591dcc
patonz <leonardo.montecchiari@tii.ae>
2022-03-24 23:33:35 +0400

    implement unicast system, improve log

--- test.js
+++ test.js
@@ -2,6 +2,8 @@ const BleDevice = require('./bleDevice')
 
 
 BleDevice.connect('/dev/ttyACM0');
+BleDevice.printUnfilteredData = false
+
 
 
 BleDevice.onReceiveMessage((payload, info)=>{
@@ -9,3 +11,9 @@ BleDevice.onReceiveMessage((payload, info)=>{
     console.log(info);
 
 })
+
+setInterval(() => {
+    BleDevice.sendUnicastMessage('ciaone1', (log)=>{
+        console.log(log)
+    }, '0xC001');
+}, 200);
